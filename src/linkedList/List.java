244f08257fd92d29b99b8b80e6c5da443d4c0ee1
nehanaisha <nehachaurasia682@gmail.com>
2022-06-14 00:03:13 +0530

    adding elements

--- src/linkedList/List.java
+++ src/linkedList/List.java
@@ -3,11 +3,31 @@ package linkedList;
 import java.util.LinkedList;
 
 public class List{
+	
+	
 	public static void main(String[] args) {
-		LinkedList<Integer> number = new LinkedList<>();
+		List obj = new List();
+		obj.create();
+		obj.add();
+	}
+		void create() {
+			LinkedList<Integer> number = new LinkedList<>();
+			
 		number.add(56);
 		number.add(30);
 		number.add(70);
 		System.out.println("linkedlist: " + number);
 	}
+		void add() {
+			LinkedList<Integer> num = new LinkedList<>();
+			
+			num.add(70);
+			System.out.println("the number added to the list is: " + num);
+			num.add(0,30);
+			System.out.println("the number added to the list is: " + num);
+			num.add(0,56);
+			System.out.println("the number added to the list is: " + num);
+			System.out.println("the final list is: " + num);
+				
+		}
 }
