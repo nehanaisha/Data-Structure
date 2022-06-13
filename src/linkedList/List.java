b696d0affe0ce2d74a74c5ba5df5ff261ac0a2e2
nehanaisha <nehachaurasia682@gmail.com>
2022-06-13 16:45:13 +0530

    linkedlist

---
+++ src/linkedList/List.java
@@ -0,0 +1,13 @@
+package linkedList;
+
+import java.util.LinkedList;
+
+public class List{
+	public static void main(String[] args) {
+		LinkedList<Integer> number = new LinkedList<>();
+		number.add(56);
+		number.add(30);
+		number.add(70);
+		System.out.println("linkedlist: " + number);
+	}
+}
